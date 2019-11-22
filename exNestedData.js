207fdd62708988359dd08362366f8bddf93ec97d
Jethet <henriette.hettinga@gmail.com>
2019-11-22 16:17:16 +0100

    Fifth exercise Nested Data Structures completed

--- exNestedData.js
+++ exNestedData.js
@@ -1,10 +1,10 @@
-/* Using the given array of objects:
-        display price of iPhone,
-        display both phones’ names,
-        add a new phone at the beginning of the array,
-        remove the last element of the array
+/* First: Using the given array of objects:
+          display price of iPhone,
+          display both phones’ names,
+          add a new phone at the beginning of the array,
+          remove the last element of the array
 */
-/*let products = [
+let products = [
     {
         name: "iPhone",
         price: 799.99
@@ -28,15 +28,14 @@ products.pop();
 
 console.log(products[0].price);
 console.log(products[0].name, products[1].name);
-console.log(products); */
+console.log(products);
 
-/*
-    Given the array, print:
-        your course type (full-time or part-time)
-        the most familiar topic
-        the least familiar topic
+/* Second:  Given the array, print:
+            your course type (full-time or part-time)
+            the most familiar topic
+            the least familiar topic
 */
-/*let course = {
+let course = {
   name: "Web Development",
   type: ["full-time", "part-time"],
   topics: ["HTML/CSS & Responsive Design", "JavaScript", "MongoDB", "Node", "Express", "React"]
@@ -46,6 +45,7 @@ console.log(course.type[0]);
 console.log(course.topics[0]);
 console.log(course.topics[5]);
 
+// Third: Given the object with nested objects in it, print:
 let student = {
   firstName: "Ana",
   lastName: "Blair",
@@ -69,6 +69,7 @@ console.log(student.address.city = "Madrid");
 // => Ana moved from Barcelona to Madrid to take part-time Web Development course.
 console.log(student);  // City has changed into Madrid.
 
+// Fourth: Given a 2D array, print the following:
 const ironCampuses = [
   ["Mexico City", "Miami", "Sao Paulo"],
   ["Amsterdam", "Barcelona", "Berlin", "Lisbon", "Madrid", "Paris"]
@@ -77,4 +78,42 @@ const ironCampuses = [
 console.log(ironCampuses[0][1]); // => Miami
 console.log(ironCampuses[1][0]); // => Amsterdam
 console.log(ironCampuses[1][5]); // => Paris
-*/
+
+// Fifth: Use the example from the lesson with frameworks to retrieve the following:
+let basic = {
+  language: "JavaScript",
+  frameworks: [
+    {
+      end: "back",
+      list: [
+        {
+          name: "ExpressJS",
+          released: 2010
+        },
+        {
+          name: "MeteorJS",
+          released: 2012
+        }
+      ]
+    },
+    {
+      end: "front",
+      list: [
+        {
+          name: "ReactJS",
+          released: 2013
+        },
+        {
+          name: "VueJS",
+          released: 2014
+        }
+      ]
+    }
+  ]
+};
+
+console.log(basic.frameworks[0].list[0].name);
+
+let firstElement = basic.frameworks[0].list[0].name;
+let secondElement = basic.frameworks[1].list[0].name;
+console.log(`At Ironhack, I'll learn ${firstElement} and ${secondElement}.`);
