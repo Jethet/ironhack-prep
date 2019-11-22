9cef7c1d3efd5e7c5626cba323218ec14cfdf045
Jethet <henriette.hettinga@gmail.com>
2019-11-22 18:01:52 +0100

    First three exercises with functions

---
+++ exFunctions.js
@@ -0,0 +1,26 @@
+/*
+// Define function welcome() that will receive one argument, your name, 
+// and greet you with your name.
+function welcome(name){
+    console.log(`Hello ${name}!`);
+}
+welcome("Frank");
+
+// Define a function printToTwenty() that will print the numbers from 0 to 20 to
+// the console ( don’t forget we have to invoke/call the function to see it working
+function printToTwenty(){
+    for(i = 0; i <= 20; i++){
+        console.log(i);
+    }
+}
+printToTwenty();
+
+// Define a function printNumbers() that receives one argument and prints the numbers
+// from 0 to whatever number we pass to it as an argument
+function printNumbers(num){
+    for(i = 0; i <= num; i++){
+        console.log(i);
+    }
+}
+printNumbers(12);
+*/
