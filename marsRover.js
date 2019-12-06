a70fd0a881afb8fe2cf7ffe259bd6b4623df716c
Jethet <henriette.hettinga@gmail.com>
2019-12-06 16:22:26 +0100

    Add moveBackwards function to Mars Rover programme

--- marsRover.js
+++ marsRover.js
@@ -16,8 +16,8 @@ function turnLeft(rover){
         rover.direction = "E";
         //console.log(rover.direction);
     } else if (rover.direction === "E"){
-            rover.direction = "N";
-            //console.log(rover.direction);
+        rover.direction = "N";
+        //console.log(rover.direction);
     }
 }
 
@@ -32,13 +32,13 @@ function turnRight(rover){
         rover.direction = "W";
         //console.log(rover.direction);
     } else if (rover.direction === "W"){
-            rover.direction = "N";
-            //console.log(rover.direction);
+        rover.direction = "N";
+        //console.log(rover.direction);
     }
 }
 
 function moveForward(rover){
-    if (rover.x >= 0 && rover.x <= 9 && rover.y >= 0 && rover.y < 9){
+    if (rover.x >= 0 && rover.x <= 9 && rover.y >= 0 && rover.y <= 9){
         if (rover.direction === "N"){
             rover.y--;
             //console.log(`Position is: ${rover.x}, ${rover.y}`);
@@ -57,6 +57,26 @@ function moveForward(rover){
     }
 }
 
+function moveBackwards(rover){
+    if (rover.x >= 0 && rover.x <= 9 && rover.y >= 0 && rover.y <= 9){
+        if (rover.direction === "N"){
+            rover.y++;
+            console.log(`Position is: ${rover.x}, ${rover.y}`);
+        } else if (rover.direction === "S"){
+            rover.y--;
+            console.log(`Position is: ${rover.x}, ${rover.y}`);
+        } else if (rover.direction === "W"){
+            rover.x--;
+            console.log(`Position is: ${rover.x}, ${rover.y}`);
+        } else if (rover.direction === "E"){
+            rover.x++;
+            console.log(`Position is: ${rover.x}, ${rover.y}`);
+        }
+    else {
+        console.log("The rover cannot be placed outside the grid.");
+    }
+    }
+}
 function manageRover(rover, directions){
     for (let i = 0; i <= directions.length; i++){
         let orientation = directions[i];
@@ -73,11 +93,16 @@ function manageRover(rover, directions){
                 moveForward(rover, orientation);
                 console.log(`Rover facing direction ${rover.direction}, x is ${rover.x} and y is ${rover.y}`);
                 break;
+            case "b":
+                moveBackwards(rover, orientation);
+                console.log(`Rover facing direction ${rover.direction}, x is ${rover.x} and y is ${rover.y}`);
+                break;
         }
     }
     rover.travelLog.push({x: rover.x, y: rover.y});
     console.log(rover.travelLog);
 }
+
 //TWO EXAMPLES:
 
 manageRover(rover, "rffffffffffff");
